ad66a853350a6063df05196f4a3cf720730bf865
Matias Poullain <matias.poullain@gmail.com>
2022-07-11 21:34:28 -0300

    mapa agri

--- mapas/mapa_mask_agri.R
+++ mapas/mapa_mask_agri.R
@@ -16,17 +16,17 @@ deptos <- st_read("mapas/data/departamentos/departamentos.shp")%>%
 
 mascara <- raster("mapas/data/mask_agri/mask_agri_aoi.tif")
 
-paleta <- wes_palette("Darjeeling1", n = uniqueN(mascara[1:10,1:10]))
+paleta <- wes_palette("Darjeeling1", n = 2)
 
 unique(mascara[])
 
 mascar_spdf <- as(mascara, "SpatialPixelsDataFrame")
-mascara_df <- as.data.frame(mascara_spdf)
+mascara_df <- as.data.frame(mascar_spdf)
 colnames(mascara_df) <- c("value", "x", "y")
 
-ggplot() +
+mapa_mask_agri <- ggplot() +
   geom_raster(data = mascara_df, aes(x = x, y = y, fill = as.factor(value))) +
-  geom_sf(data = deptos) +
+  geom_sf(data = deptos, fill = "transparent") +
   coord_sf(label_axes = "ENEN") +
   theme_bw() +
   annotation_scale(
@@ -37,6 +37,9 @@ ggplot() +
     pad_x = unit(0.4, "in"), pad_y = unit(0.4, "in"),
     style = north_arrow_fancy_orienteering()) +
   scale_x_continuous(labels = function(x) paste0(abs(x), "°O")) +
-  scale_fill_manual(values = paleta) +
-  labs(fill = str_wrap("Municipios/Partido de la zona de estudio", 25))+
+  scale_fill_manual(values = paleta, label = c("No cultivable", "Cultivable")) +
+  labs(fill = "Tipo de área")+
   theme(axis.title = element_blank())
+
+ggsave("mapas/figuras/mapa_mask_agri.png", mapa_mask_agri, width = 8, heigh = 8)
+
