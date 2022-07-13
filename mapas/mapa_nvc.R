2b1e82209c72f970490dfbd4eaa5f2a48fe02cb0
Matias Poullain <matias.poullain@gmail.com>
2022-07-13 11:24:39 -0300

    mapa nvc

--- mapas/mapa_nvc.R
+++ mapas/mapa_nvc.R
@@ -24,6 +24,8 @@ for(i in archivos){
   }
 }
 
+writeRaster(r, "data/merge_nvc", format = "GTiff")
+
 r_df <- as(r, "SpatialPixelsDataFrame")
 r_df <- as.data.table(r_df)
 colnames(r_df) <- c("value", "x", "y")
@@ -32,6 +34,7 @@ r_df[value == -111, "value"] <- NA_integer_
 
 r_df[, value := as.factor(value)]
 
+paleta <- wes_palette("Cavalcanti1", n = 2)
 
 
 
@@ -48,13 +51,33 @@ mapa_nvc <- ggplot() +
     pad_x = unit(0.4, "in"), pad_y = unit(0.4, "in"),
     style = north_arrow_fancy_orienteering()) +
   scale_x_continuous(labels = function(x) paste0(abs(x), "°O")) +
-  scale_fill_nejm()+#label = c("No cultivable", "Cultivable")) +
-  labs(fill = "Clasificación de la nueva verdad de campo")+
+  scale_fill_nejm(label = c("", "No cultivable", "Cultivable")) +
+  labs(fill = str_wrap("Clasificación de la nueva verdad de campo", 20))+
   theme(axis.title = element_blank())
 
-ggsave("mapas/figuras/mapa_nvc.png", mapa_nvc, width = 8, heigh = 8)
+ggsave("figuras/mapa_nvc.png", mapa_nvc, width = 8, heigh = 7)
 
 
+r_df[, value := fifelse(value == "-99", "No", "Sí")]
+
+r_df <- r_df[!is.na(value)]
 
 
+mapa_nvc_dic <- ggplot() +
+  geom_raster(data = r_df, aes(x = x, y = y, fill = as.factor(value))) +
+  geom_sf(data = deptos, fill = "transparent") +
+  coord_sf(label_axes = "ENEN") +
+  theme_bw() +
+  annotation_scale(
+    location = "br",
+    bar_cols = c("grey60", "white")) +
+  annotation_north_arrow(
+    location = "tr", which_north = "true",
+    pad_x = unit(0.4, "in"), pad_y = unit(0.4, "in"),
+    style = north_arrow_fancy_orienteering()) +
+  scale_x_continuous(labels = function(x) paste0(abs(x), "°O")) +
+  scale_fill_manual(values = paleta,label = c("", "No cultivable", "Cultivable")) +
+  labs(fill = str_wrap("Clasificación como nueva verdad de campo", 20))+
+  theme(axis.title = element_blank())
 
+ggsave("figuras/mapa_nvc_dic.png", mapa_nvc_dic, width = 8, heigh = 7)
